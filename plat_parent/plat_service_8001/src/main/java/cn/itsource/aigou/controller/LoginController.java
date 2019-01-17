553e948a2af4dc15865495c92ad5b4843654b988
wilson <309860067@qq.com>
2019-01-17 16:09:22 +0800

    12331242

--- plat_parent/plat_service_8001/src/main/java/cn/itsource/aigou/controller/LoginController.java
+++ plat_parent/plat_service_8001/src/main/java/cn/itsource/aigou/controller/LoginController.java
@@ -18,9 +18,9 @@ public class LoginController {
 
         //正常来说需要到数据库通过用户名查询用户,查询出来判断是否为空,如果不为空还要比对密码
         //现在我们先模拟,只有用户名为admin,密码为0才能登陆
-        if ("admin".equals(employee.getName())&&"0".equals(employee.getPassword())){
+        if ("admin".equals(employee.getName())&&"123456".equals(employee.getPassword())){
             return AjaxResult.me();
         }
-        return AjaxResult.me().setSuccess(false).setMessage("用户名或密码不正确!");
+        return AjaxResult.me().setSuccess(false).setMessage("用户名或密码不正确!").setResultObj(employee);
     }
 }
